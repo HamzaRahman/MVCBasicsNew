463b058a976430a6e4ad937204475e2c2ef019de
Hamza Rahman <hamza.rahman2016@outlook.com>
2021-09-15 14:33:03 +0500

    MVC Data 1--Task 4

---
+++ MVCBasicsNew/Repository/IPeopleRepo.cs
@@ -0,0 +1,23 @@
+﻿using MVCBasicsNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasicsNew.Repository
+{
+    public interface IPeopleRepo
+    {
+        //        IPeopleRepo – Interface with following methods.
+        //          ▪ Person Create(“parameters needed to create Person excluding id”)
+        //          ▪ List<Person> Read()
+        //          ▪ Person Read(int id)
+        //          ▪ Person Update(Person person)
+        //          ▪ bool Delete(Person person)
+        Person Create(string Name, int PhoneNumber, string City);
+        List<Person> Read();
+        Person Read(int ID);
+        Person Update(Person person);
+        bool Delete(Person person);
+    }
+}
