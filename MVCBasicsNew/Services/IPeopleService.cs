a856c968df623559bd4a966f929594b5d2c1a350
Hamza Rahman <hamza.rahman2016@outlook.com>
2021-09-15 14:38:53 +0500

    MVC Data 1--Task 6

---
+++ MVCBasicsNew/Services/IPeopleService.cs
@@ -0,0 +1,25 @@
+﻿using MVCBasicsNew.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasicsNew.Services
+{
+    public interface IPeopleService
+    {
+        //IPeopleService – Interface with following methods.
+        //▪ Person Add(CreatePersonViewModel person)
+        //▪ PeopleViewModel All()
+        //▪ PeopleViewModel FindBy(PeopleViewModel search)
+        //▪ Person FindBy(int id)
+        //▪ Person Edit(int id, Person person)
+        //▪ bool Remove(int id)
+        Person Add(CreatePersonViewModel person);
+        PeopleViewModel All();
+        PeopleViewModel FindBy(PeopleViewModel Search);
+        Person FindBy(int ID);
+        Person Edit(int ID, Person person);
+        bool Remove(int ID);
+    }
+}
