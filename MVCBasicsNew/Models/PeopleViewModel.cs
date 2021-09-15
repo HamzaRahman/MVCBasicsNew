0477e6f4d85fea00c1b0ff5389b0eeb75f6c8f9d
Hamza Rahman <hamza.rahman2016@outlook.com>
2021-09-15 14:28:36 +0500

    MVC Data 1--Task 3

---
+++ MVCBasicsNew/Models/PeopleViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCBasicsNew.Models
+{
+    public class PeopleViewModel
+    {
+        // PeopleViewModel – container for the information you need in your people view.
+        public PeopleViewModel()
+        {
+
+        }
+        public List<Person> people = new List<Person>();
+        public string SearchPhrase { get; set; }
+        public CreatePersonViewModel CreatePerson { get; set; }
+    }
+}
